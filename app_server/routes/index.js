c9a790317fad73febe311e62bee5aff883fef9b7
Elizabeth Zweizig <elizabethzweizig@hannahs-iphone.pugetsound.edu>
2017-01-20 10:33:20 -0800

    Four links created for About, Locations, Reviews and Homepage

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
-var homepageController = function(req, res) {
-  res.render('index', { title: 'Express'});
-};
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
-router.get('/', homepageController);
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+  /* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
