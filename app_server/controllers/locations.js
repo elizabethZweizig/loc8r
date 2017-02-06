0fff267a31cab6b6014f926975ca78595245e9b3
Elizabeth Zweizig <ezweizig@pugetsound.edu>
2017-02-05 16:16:50 -0800

    Ch4 all views pages now functional

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ module.exports.homelist = function(req, res){
 };
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res){
-  res.render('location-info', { title: 'Location info' });
+  res.render('locations-info', { title: 'Location info' });
 };
 
 /* GET 'Add review' page */
